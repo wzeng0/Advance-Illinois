3e4669ec55f45f8f01f7eb53c0ded0e55989e303
Clarice Kim <claricekim@Clarices-MacBook-Air.local>
2023-04-23 11:13:41 -0500

    testing pdf creation in browser

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import BasicDocument from "./pdf-create";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <BasicDocument />
     </div>
   );
 }
